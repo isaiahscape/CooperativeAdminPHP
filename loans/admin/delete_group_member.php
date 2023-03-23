bbdcf3ccc1f03b646a7cdd77be2df44869856853
Isaiah Rafael <lqlp0011@gmail.com>
2023-03-23 15:30:09 +0800

    go

--- loans/admin/delete_group_member.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include('../connection.php');
-
-
-$q=mysqli_query($conn,"delete from member where member_id='".$_GET['id']."'");
-
-header('location:index.php?page=display_member');
-
-?>
--- loans/admin/delete_loan_record.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include('../connection.php');
-
-
-mysqli_query($conn,"delete from loan where loan_id='".$_GET['id']."'");
-
-header('location:index.php?page=display_loan');
-
-?>
